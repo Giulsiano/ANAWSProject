3926a6c34c8eb7e0775d8db3d25926fed4b185fe
Giuliano Peraz <giuliano.peraz@gmail.com>
2018-02-10 13:48:56 +0100

    Implemented getRouterList
    This function will return the list of all routers inside the area where the tool is attached to.

--- src/Functions.java
+++ src/Functions.java
@@ -9,16 +9,23 @@ import net.schmizz.sshj.connection.channel.direct.Session;
 import net.schmizz.sshj.connection.channel.direct.Session.Command;
 
 public class Functions {
-
+	
+	static final String USER = "cisco";
+	static final String PASSWORD = "cisco"
+	static final String[] VTYCOMMAND = {"/usr/bin/vtysh", "-d", "ospfd", "-c", "show ip ospf database"};
+	
 	public static void connectToRouter() throws IOException {
 			System.out.println("Connect to router, This is a test\n");
-			System.exit(0); //just for testing
-		//provvisorio ma penso rimarrà in questa posizione
+			System.out.println("Getting router list");
+			ArrayList<String> routers = getRouterList();
+			if (routers == null) {
+				System.err.println("Error retrieving list of router");
+			}
 	        final SSHClient ssh = new SSHClient();
 	        ssh.loadKnownHosts();
 	        ssh.connect("localhost");
 	        try {
-	        	String[] command = {"/usr/bin/vtysh", "-d", "ospfd", "-c", "show ip ospf database"};
+	        	String[] command = 
 	            ssh.authPassword("user", "user");
 	            Runtime rt = Runtime.getRuntime();
 	            Process vtysh = rt.exec(command, null, null);
@@ -55,6 +62,34 @@ public class Functions {
 	
 	public static void showRunningConf() {
 		System.out.println("Show running configurations, This is a test\n");
+
+	}
+	
+	/**
+	 * Get the list of the router id from the ospfd daemon
+	 * @return List of router id. They are the IP ssh will connect to or null if the command fails
+	 */
+	private List<String> getRouterList(){
+		try {
+			List<String> routers == null;
+			Runtime rt = Runtime.getRuntime();
+			while(routers == null) {
+				Process vtysh = rt.exec(VTYCOMMAND);
+				if (vtysh == null) {
+					System.err.println("Error in command string"));
+					return null;
+				}
+				routers = getRouterIds(vtysh.getInputStream());
+			}
+			return routers;
+		}
+		catch (Exception e) {
+			System.err.printf("Exception caught: %s\n%s\n", e.getClass().getName(), e.getMessage());
+			return null;
+		}
+	}
+	
+	private List<String> getRouterIds(InputStream s){
 		
 	}
 }
