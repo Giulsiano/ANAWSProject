15a8f01ac1d636b2b68ba1c9d970dcf7a02f83db
Giuliano Peraz <giuliano.peraz@gmail.com>
2018-02-14 10:26:41 +0100

    Added a stub for show running configs

--- src/Functions.java
+++ src/Functions.java
@@ -1,22 +1,22 @@
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.schmizz.sshj.SSHClient;
-import net.schmizz.sshj.common.DisconnectReason;
 import net.schmizz.sshj.common.IOUtils;
 import net.schmizz.sshj.common.LoggerFactory;
 import net.schmizz.sshj.common.StreamCopier;
@@ -24,7 +24,6 @@ import net.schmizz.sshj.connection.ConnectionException;
 import net.schmizz.sshj.connection.channel.direct.Session;
 import net.schmizz.sshj.connection.channel.direct.Session.Command;
 import net.schmizz.sshj.connection.channel.direct.Session.Shell;
-import net.schmizz.sshj.transport.DisconnectListener;
 import net.schmizz.sshj.transport.TransportException;
 import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
 
@@ -35,8 +34,9 @@ public class Functions{
 	static final String[] VTYCOMMAND = {"/usr/bin/vtysh", "-d", "ospfd", "-c", "show ip ospf database"};
 	private Set<String> localAddress;
 	private String topology;
-	
+	private Runtime rt;
     public Functions() {
+    	rt = Runtime.getRuntime();
 		localAddress = new HashSet<String>();
 		// get address of all interfaces of this machine
 		try {
@@ -61,59 +61,59 @@ public class Functions{
 	}
 	
 	public void connectToRouter() throws IOException {
-			String userInput = null;
-			System.out.println("Connect to router, This is a test\n");
-			System.out.println("Getting router list");
-			List<String> routers = getRouterList();
-			if (routers == null) {
-				System.err.println("Error retrieving list of router");
+		String userInput = null;
+		System.out.println("Connect to router, This is a test\n");
+		System.out.println("Getting router list");
+		List<String> routers = getRouterList();
+		if (routers == null) {
+			System.err.println("Error retrieving list of router");
+		}
+		
+		// Print router list and make the user to choose
+		System.out.println("List of routers:");
+		for(String ip: routers) {
+			System.out.println(ip);
+		}
+		while(userInput == null) {
+			System.out.println("Choose the router IP of the list to which you want to connect to:");
+			userInput = System.console().readLine();
+			if (!routers.contains(userInput)) {
+				userInput = null;
 			}
-			
-			// Print router list and make the user to choose
-			System.out.println("List of routers:");
-			for(String ip: routers) {
-				System.out.println(ip);
-			}
-			while(userInput == null) {
-				System.out.println("Choose the router IP of the list to which you want to connect to:");
-				userInput = System.console().readLine();
-				if (!routers.contains(userInput)) {
-					userInput = null;
-				}
-			}
-			
-			// Connect to the router
-			final SSHClient ssh = new SSHClient();
-	        // we don't need to verify the host. This is a toy tool.
-	        ssh.addHostKeyVerifier(new PromiscuousVerifier());
-	        ssh.connect(userInput);
-	        try {
-	            ssh.authPassword(USER, PASSWORD);
-	            final Session session = ssh.startSession();
-	            session.allocateDefaultPTY();
-	            final Shell shell = session.startShell();
-	            try {
-	            	// Redirect input and output stream in order to send user's command to the router
-	                new StreamCopier(shell.getInputStream(), System.out, LoggerFactory.DEFAULT)
-	                        .bufSize(shell.getLocalMaxPacketSize())
-	                        .spawn("stdout");
+		}
+		
+		// Connect to the router
+		final SSHClient ssh = new SSHClient();
+        // we don't need to verify the host. This is a toy tool.
+        ssh.addHostKeyVerifier(new PromiscuousVerifier());
+        ssh.connect(userInput);
+        try {
+            ssh.authPassword(USER, PASSWORD);
+            final Session session = ssh.startSession();
+            session.allocateDefaultPTY();
+            final Shell shell = session.startShell();
+            try {
+            	// Redirect input and output stream in order to send user's command to the router
+                new StreamCopier(shell.getInputStream(), System.out, LoggerFactory.DEFAULT)
+                        .bufSize(shell.getLocalMaxPacketSize())
+                        .spawn("stdout");
 
-	                new StreamCopier(shell.getErrorStream(), System.err, LoggerFactory.DEFAULT)
-	                        .bufSize(shell.getLocalMaxPacketSize())
-	                        .spawn("stderr");
-
-	                new StreamCopier(System.in, shell.getOutputStream(), LoggerFactory.DEFAULT)
-            				.bufSize(shell.getRemoteMaxPacketSize())
-            				.copy();
-	            }
-	            catch (TransportException | ConnectionException e){
-	            	System.err.printf("Exception caught: %s\n%s\n", e.getClass().getName(), e.getMessage());
-	            } finally {
-	                session.close();
-	            }
-	        } finally {
-	            ssh.disconnect();
-	        }
+                new StreamCopier(shell.getErrorStream(), System.err, LoggerFactory.DEFAULT)
+                        .bufSize(shell.getLocalMaxPacketSize())
+                        .spawn("stderr");
+                new StreamCopier(System.in, shell.getOutputStream(), LoggerFactory.DEFAULT)
+        				.bufSize(shell.getRemoteMaxPacketSize())
+        				.copy();
+                shell.close();
+            }
+            catch (TransportException | ConnectionException e){
+            	System.err.printf("Exception caught: %s\n%s\n", e.getClass().getName(), e.getMessage());
+            } finally {
+                session.close();
+            }
+        } finally {
+            ssh.disconnect();
+        }
 	}
 	
 	/**
@@ -123,8 +123,7 @@ public class Functions{
 		System.out.println("Show Topology, This is a test\n");
 		while(topology == null || topology.isEmpty()) {
 			try {
-				Runtime rt = Runtime.getRuntime();
-				Process vtysh = rt.exec(VTYCOMMAND);
+				Process vtysh = this.rt.exec(VTYCOMMAND);
 				if (vtysh == null) {
 					System.err.println("Error in command string");
 					System.exit(1);
@@ -152,7 +151,34 @@ public class Functions{
 	
 	public void showRunningConf() {
 		System.out.println("Show running configurations, This is a test\n");
-
+		String command = "show running-config";
+		Map<String, String> routerConfigs = new HashMap<String, String>();
+		try {
+			List<String> routers = getRouterIds(this.rt.exec(VTYCOMMAND).getInputStream());
+			String config = null;
+			for (String ip : routers) {
+				SSHClient ssh = new SSHClient();
+				ssh.addHostKeyVerifier(new PromiscuousVerifier());
+				try {
+					ssh.authPassword(USER, PASSWORD);
+					ssh.connect(ip);
+					Session session = ssh.startSession(); 
+					try {
+						Command cmd = session.exec(command);
+						cmd.join();
+					}
+					finally {
+						session.close();
+					}
+				}
+				finally {
+					ssh.disconnect();
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -209,11 +235,6 @@ public class Functions{
 			}
 		}
 		return routerIds;
-	}
-
-	@Override
-	public void notifyDisconnect(DisconnectReason reason, String message) {
-		System.out.printf("Disconnected - %s", reason);
 	}
 }
 
